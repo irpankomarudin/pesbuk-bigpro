75528634a1f84e9d73e9487fdaf5fe415f78b7ee
Irpan Komarudin <32387258+irpankomarudin@users.noreply.github.com>
2020-11-10 08:59:34 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $db_host = getenv("DB_HOST");
 $db_user = getenv("DB_USER");
 $db_pass = getenv("DB_PASS");
-$db_name = getenv("DB_NAME");
+$db_name = 'db_pesbuk';
 
 try {    
     //create PDO connection 
